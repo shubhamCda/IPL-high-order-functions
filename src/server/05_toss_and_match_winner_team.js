f72bf6021e5f293d7209c45182ef9536ff71e2fa
Shubham <shubham@gmail.com>
2024-09-23 08:48:45 +0530

    Fix function

--- src/server/05_toss_and_match_winner_team.js
+++ src/server/05_toss_and_match_winner_team.js
@@ -6,16 +6,17 @@ import fs from 'fs';
 
 const toss_and_match_winner_count = matches.reduce((won, { toss_winner, winner }) =>{
     if (toss_winner === winner) {
-        if (!won[winner]) {
-            won[winner] = 1;
-        } else {
-            won[winner] += 1;
-        }
+        won[winner] = (won[winner] || 0) + 1;
+        // if (!won[winner]) {
+        //     won[winner] = 1;
+        // } else {
+        //     won[winner] += 1;
+        // }
     }
     return won;
 },{});
 
-// console.log(toss_and_match_winner_count);
+console.log(toss_and_match_winner_count);
 
 
-fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL_New/src/public/output/05_toss_and_match_winner_team.json', JSON.stringify(toss_and_match_winner_count, null, 2));
+// fs.writeFileSync('/home/shubham/Desktop/Projetc/IPL_New/src/public/output/05_toss_and_match_winner_team.json', JSON.stringify(toss_and_match_winner_count, null, 2));
